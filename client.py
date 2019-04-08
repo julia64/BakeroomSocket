a60118f41a01975e45edf3e642003c1e6212e6a0
julia64 <945159252@qq.com>
2019-04-08 13:57:32 +0800

    服务端接收数据返回00

--- client.py
+++ client.py
@@ -1,19 +1,62 @@
-#-*-coding:utf-8
+# coding=utf-8
+from twisted.internet.protocol import Protocol, ClientFactory
+from twisted.internet import reactor
+import threading
+import time
+import sys
+import datetime
 
 
-from twisted.internet import protocol, reactor
+class Echo(Protocol):
+    def __init__(self):
+        self.connected = False
 
+    def connectionMade(self):
+        self.connected = True
 
-class Echo(protocol.Protocol):
+    def connectionLost(self, reason):
+        self.connected = False
+
     def dataReceived(self, data):
-        # As soon as any data is received, write it back
-        self.transport.write(data)
+        print(data.decode("utf-8"))
 
 
-class EchoFactory(protocol.Factory):
+class EchoClientFactory(ClientFactory):
+    def __init__(self):
+        self.protocol = None
+
+    def startedConnecting(self, connector):
+        print("Start to Connect...")
+
     def buildProtocol(self, addr):
-        return Echo()
+        print("Connected...")
+        self.protocol = Echo()
+        return self.protocol
+
+    def clientConnectionLost(self, connector, reason):
+        print("Lost connection. Reason: ", reason)
+
+    def clientConnectionFailed(self, connector, reason):
+        print("Connection is failed, Reason: ", reason)
 
 
-reactor.listenTCP(8000, EchoFactory())
+bStop = False
+
+
+def routine(factory):
+    while not bStop:
+        if factory.protocol and factory.protocol.connected:
+            factory.protocol.transport.write("hello, I'm %s %s" % (
+                sys.argv[0], datetime.datetime.now()
+            ))
+            print(sys.argv[0], datetime.datetime.now())
+        time.sleep(5)
+
+
+host = '127.0.0.1'
+port = 8000
+factory = EchoClientFactory()
+reactor.connectTCP(host, port, factory)
+threading.Thread(target=routine, args=(factory,)).start()
 reactor.run()
+bStop = True
--- server.py
+++ server.py
@@ -1,32 +1,49 @@
-#-*-coding:utf-8-*-
+#!/usr/bin/env python
+# coding: utf-8
 
-from twisted.internet import reactor, protocol
-
-
-class EchoClient(protocol.Protocol):
-    def connectionMade(self):
-        self.transport.write("hello, world!".encode('utf-8'))
-
-    def dataReceived(self, data):
-        print("Server said:", data)
-        self.transport.loseConnection()
-
-    def connectionLost(self, reason):
-        print("connection lost")
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import Factory
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet import reactor
 
 
-class EchoFactory(protocol.ClientFactory):
-    def buildProtocol(self, addr):
-        return EchoClient()
+class Echo(Protocol):
 
-    def clientConnectionFailed(self, connector, reason):
-        print("Connection failed - goodbye!")
-        reactor.stop()
+        # 协议类实现用户的服务协议，例如 http,ftp,ssh 等
+        def __init__(self, factory):
+                self.factory = factory
 
-    def clientConnectionLost(self, connector, reason):
-        print("Connection lost - goodbye!")
-        reactor.stop()
+        def connectionMade(self):
+
+                # 连接建立时被回调的方法
+
+                self.factory.numProtocols = self.factory.numProtocols + 1
+                self.transport.write("Welcome! There are currently %d open connections.\n".encode('utf-8')
+                                     % self.factory.numProtocols)
+
+        def connectionLost(self, reason):
+
+                # 连接关闭时被回调的方法
+                self.factory.numProtocols = self.factory.numProtocols - 1
+
+        def dataReceived(self, data):
+
+                # 接收数据的函数，当有数据到达时被回调
+                self.transport.write('00'.encode('utf-8'))
 
 
-reactor.connectTCP("localhost", 8000, EchoFactory())
-reactor.run()
+class EchoFactory(Factory):
+
+        #  协议工厂类，当客户端建立连接的时候，创建协议对象，协议对象与客户端连接一一对应
+        numProtocols = 0
+
+        def buildProtocol(self, addr):
+                return Echo(self)
+
+
+if __name__ == '__main__':
+        # 创建监听端口
+        reactor.listenTCP(8007, EchoFactory())
+
+        # 开始监听事件
+        reactor.run()
